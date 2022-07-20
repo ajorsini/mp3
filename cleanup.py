2eb1855b7117053714267dae0e0c46e649922482
aorsini <alejandro_orsini@cargill.com>
2022-07-20 13:17:13 -0300

    fisrt commit

---
+++ cleanup.py
@@ -0,0 +1,34 @@
+
+
+
+schema = StructType() \
+      .add("RecordNumber",IntegerType(),True) \
+      .add("Zipcode",IntegerType(),True) \
+      .add("ZipCodeType",StringType(),True) \
+      .add("City",StringType(),True) \
+      .add("State",StringType(),True) \
+      .add("LocationType",StringType(),True) \
+      .add("Lat",DoubleType(),True) \
+      .add("Long",DoubleType(),True) \
+      .add("Xaxis",IntegerType(),True) \
+      .add("Yaxis",DoubleType(),True) \
+      .add("Zaxis",DoubleType(),True) \
+      .add("WorldRegion",StringType(),True) \
+      .add("Country",StringType(),True) \
+      .add("LocationText",StringType(),True) \
+      .add("Location",StringType(),True) \
+      .add("Decommisioned",BooleanType(),True) \
+      .add("TaxReturnsFiled",StringType(),True) \
+      .add("EstimatedPopulation",IntegerType(),True) \
+      .add("TotalWages",IntegerType(),True) \
+      .add("Notes",StringType(),True)
+
+df_with_schema = spark.read.format("csv") \
+      .option("header", True) \
+      .schema(schema) \
+      .load("/tmp/resources/zipcodes.csv")
+
+
+dropDisDF = df.dropDuplicates(["department","salary"])
+print("Distinct count of department & salary : "+str(dropDisDF.count()))
+dropDisDF.show(truncate=False)
